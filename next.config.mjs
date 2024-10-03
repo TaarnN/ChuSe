c5f0875c10b19d558974bec6c888e840e86e5240
TaarnN <157127790+TaarnN@users.noreply.github.com>
2024-10-03 19:01:42 +0700

    asdf 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  exportPathMap: async function () {
-    return {
-      "/sitemap.xml": { page: "/sitemap.xml" },
-    };
-  },
 };
 
 export default nextConfig;
