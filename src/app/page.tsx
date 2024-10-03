063abcab77807ba402e42378d41f85e98b199ddd
TaarnN <157127790+TaarnN@users.noreply.github.com>
2024-10-03 20:20:10 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,11 +99,11 @@ export default function Home() {
                 Churairat SE google api url (html):{" "}
               </p>
               <a
-                href={"http://localhost:3000/api/google?query=" + query}
+                href={"https://churairatse.vercel.app/api/google?query=" + query}
                 target="_blank"
                 className="text-emerald-500"
               >
-                {"http://localhost:3000/api/google?query=" + query}
+                {"https://churairatse.vercel.app/api/google?query=" + query}
               </a>
             </div>
             <div className="ml-8 flex">
@@ -111,11 +111,11 @@ export default function Home() {
                 Churairat SE search api url (links):{" "}
               </p>
               <a
-                href={"http://localhost:3000/api/search?query=" + query}
+                href={"https://churairatse.vercel.app/api/search?query=" + query}
                 target="_blank"
                 className="text-emerald-500"
               >
-                {"http://localhost:3000/api/search?query=" + query}
+                {"https://churairatse.vercel.app/api/search?query=" + query}
               </a>
             </div>
             <div className="ml-8 flex">
